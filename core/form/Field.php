663f90343e45e7e9bcb13b87d84aea295f8b2e37
Simone Maccari <s.maccari@geosystems.it>
2023-06-23 15:52:39 +0200

    model form and field

--- core/form/Field.php
+++ core/form/Field.php
@@ -1,12 +1,17 @@
 <?php
+
 namespace app\core\form;
+
 use app\core\Model;
+
 class Field
 {
     public Model $model;
     public string $attribute;
-    public function __construct(Model $model, $attribute) {
+    public function __construct(Model $model, $attribute)
+    {
         $this->model = $model;
         $this->attribute = $attribute;
     }
+    
 }
