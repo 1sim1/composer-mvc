1ba824e7e194df76df49a1c6766dbc7254fb8f3c
Simone Maccari <s.maccari@geosystems.it>
2023-06-23 17:32:08 +0200

    routing mvc

--- views/login.php
+++ views/login.php
@@ -2,11 +2,11 @@
 <form action="" method="post">
   <div class="mb-3">
     <label>e-Mail</label>
-    <input type="email" name="email" class="form-control">
+    <input type="email" id="email" name="email" class="form-control">
   </div>
   <div class="mb-3">
     <label>Password</label>
-    <input type="password" name="password" class="form-control">
+    <input type="password" id="password" name="password" class="form-control">
   </div>
   <button type="submit" id="login" class="btn btn-primary">Submit</button>
 </form>
--- web/js/LoginManager.js
+++ web/js/LoginManager.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    $('#login').off('click').on('click', onClickLogin);
+    $('#login').off('click').on('click', onSubmitLogin);
 });
+
+onSubmitLogin = function() {
+    let email = $('#email').val();
+    alert('Login con email ' + email);
+}
